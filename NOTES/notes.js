d282c26dbd61053ea07244e44e4631f8838b4855
ayushchaudhari562 <ayushchaudhry562@gmail.com>
2026-01-07 08:58:51 +0530

    module

--- NOTES/notes.js
+++ NOTES/notes.js
@@ -1,1 +1,41 @@
 // lect 1 and 2 ,3;
+
+
+/* Frontend (Browser)
+HTML + CSS + JS
+        ↓
+Backend (Node.js)
+Business Logic + APIs
+        ↓
+Database
+MongoDB / MySQL / PostgreSQL
+*/
+
+/* why we use NODE ?
+
+    - Same JS for frontend + backend
+
+    - Very fast (non-blocking)
+    - Good for real-time apps (games, chat, APIs)
+    */
+
+/* modules in NodeJS    
+
+    module in Node.js is a reusable block of code stored in a file.
+
+    to export : module.export = " object  "
+    module.export = {
+        fun1;
+        fun2;
+        fun3;
+    }*/
+
+    // to get or import: 
+    const math = require("./current file name");
+    const {fun1,fun2} = require("./from where you export");
+    console.log(call);
+
+    // there are also many why to import and export module;
+    
+     
+    
